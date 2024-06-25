084fbbad95cad1c47a1c339ad987be73357fce8e
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-06-25 10:02:10 +0000

    variables and data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3)  // we are using nodejs, not browser
+
+console.log( 3
+    + 
+    3) // code readability shoud be high
+
+   console.log("Hitesh") 
+
+   let name = "sandeep"
+   let age = 27
+   let isLoggedIn = false
+
+       //primitive datatype
+   // number => 2 to power 53
+   // bigint 
+   // string => " "
+   // boolean => true/false
+   // null => standalone value
+   // undefined => value is not assigned 
+   // symbol => unique
+
+      // Non-primitive data type
+  // object
+
+
+  console.log(typeof undefined); //undefined
+  console.log(typeof null); // object
