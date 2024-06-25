8b8385091f318ed790823dff069f907d762af16d
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-06-25 11:07:30 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false=> 0
+
+let isLoggedIn = "sand"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => ture; 0 => flase
+// "" => false
+// "sand" => ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+ 
