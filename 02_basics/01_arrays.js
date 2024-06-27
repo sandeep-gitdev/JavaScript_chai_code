3d6da8ca7154061bea8b2c64a0c0f5fc59cd2b4f
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-06-27 08:11:17 +0000

    arrays in js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -25,14 +25,14 @@
 
   // slice, splice
 
-  console.log("A ", myArr);
+ // console.log("A ", myArr);
 
   const myn1 = myArr.slice(1,3);
 
-  console.log(myn1);
-  console.log("B ", myArr);
+//   console.log(myn1);
+//   console.log("B ", myArr);
 
   const myn2 = myArr.splice(1,3);
-  console.log("C ", myArr);
-  console.log(myn2);
+//   console.log("C ", myArr);
+//   console.log(myn2);
 
