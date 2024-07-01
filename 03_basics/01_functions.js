eb804ecc55ab4632d018201a2c855feb04e82111
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-07-01 05:11:55 +0000

    function with object and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,4 +38,39 @@ function sayMyName() {
  }
 
  //console.log(loginUserMessage("sandeep"));    //to print retrun value or print it
-   console.log(loginUserMessage());
+ //  console.log(loginUserMessage());
+
+   function calculateCartPrice(val1, val2, ...num1) {      // (... is rest opertor)  pass multipale value in functions
+          return num1
+   }
+
+   //console.log(calculateCartPrice(200, 400, 100, 900));
+
+   const user = {
+        username: "sandeep",
+        price: 199
+   }
+           // passing the object in function
+
+     function handleObject(anyobject){
+      //  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+     }
+
+     //handleObject(user)
+
+     handleObject({
+        username: "sam",
+        price: 499
+     });
+
+             // passing the Array in function
+
+             const myNewArray = [200, 400, 700]
+
+             function returnSecondValue(getArray) {
+                return getArray[2]
+             }
+
+            // console.log(returnSecondValue(myNewArray));
+
+            console.log(returnSecondValue([200,400, 800, 500]));
