39404e6ee1385528906a7f74e02010831ade1c8b
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-07-03 12:34:23 +0000

    control_flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+//  switch (key) {
+//     case value:
+        
+//         break;
+ 
+//     default:
+//         break;
+//  }
+
+const month = "March"
+
+  switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+  }
+
+
